33fa4918fd57ebcc0461d8d3cff4b8caba6d93d5
Kayla Lynch <kalynch1292@gmail.com>
2020-11-02 21:07:43 -0500

    working on everything

--- develop/script.js
+++ develop/script.js
@@ -1,18 +1,77 @@
-// Assignment Code
+// Assignment Code//
 var generateBtn = document.querySelector("#generate");
+
+
+
 var possibleChars = [];
-var arrayOfUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var arrayOfLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var arrayOfSymbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
-var arrayOfNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+// return arrayOf{Upper} -- random pick in array of each function//
+function getUpper() {
+  var arrayOfUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  return arrayOfUpper[Math.floor(Math.Random() * 26)]
+}
+
+function getLower() {
+  var arrayOfLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  return arrayOfLower[Math.floor(Math.random() * 26)]
+}
+
+function getSymbols() {
+  var arrayOfSymbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"]
+  return arrayOfSymbols[Math.floor(Math.random() * 13)]
+}
+
+function getNumbers() {
+  var arrayOfNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  return arrayOfNumbers[Math.floor(Math.random() * 9)]
+}
 
 
 
-//// while loop ///
-function writePassword() {
-  var desiredLength = prompt("How long do you want your password? (Between 8-120 characters)")
-  while (i < 8 || i > 128) {
-    desiredLength = prompt("How long do you want your password? (Between 8-120 characters)")
+// Prompt length //
+function writePassword(h) {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+function generatePassword() {
+  // var password = '';
+
+  var desiredLength = parseInt(prompt("How long do you want your password? (Between 8-128 characters)"))
+  while (desiredLength < 8 || desiredLength > 128) {
+    alert("password must be between 8-128 characters")
+    desiredLength = prompt("How long do you want your password? (Between 8-128 characters)")
+  }
+
+  var userWantsUpper = confirm('Do you want to include uppercase letters?')
+  var userWantsLower = confirm('Do you want to include lowercase letters?')
+  var userWantsSymbols = confirm('Do you want to include Numbers?')
+  var userWantsNumbers = confirm('Do you want to include Symbols?')
+
+  if (!userWantsUpper && !userWantsLower && !userWantsSymbols && !userWantsNumbers) {
+    alert("You must choose at least one");
+    return
+  }
+
+
+
+
+  var confirmStyle
+  if (userWantsUpper) {
+    possibleChars.push(arrayOfUpper)
+  }
+  if (userWantsLower) {
+    possibleChars.push(arrayOfLower)
+  }
+  if (userWantsSymbols) {
+    possibleChars.push(arrayOfSymbols)
+  }
+
+  if (userWantsNumbers) {
+    possibleChars.push(arrayOfNumbers)
   }
 }
 
@@ -21,38 +80,22 @@ function writePassword() {
 
 
 
+//   for (var i = 0; i < desiredLength; i++) {
+//     var charset = possibleChars[Math.floor(Math.random() * possibleChars.length)];
+//     var char = charset[Math.floor(Math.Random() * charset.length)];
+//     result += char;
+//   }
+//   return result;
+// }
 
 
-// help//
-var confirmStyle  
-if (userWantsUpper) {
-  possibleChars.push(arrayOfUpper)
-}
-if (userWantsLower) {
-  possibleChars.push(arrayOfLower)
-}
-if (userWantsSymbols) {
-  possibleChars.push(arrayOfSymbols)
-}
 
-if (userWantsNumbers) {
-  possibleChars.push(arrayOfNumbers)
-}
 
 
 
 /////// 
 
 
-function genPassword() {
-  var result = [];
-  for (var i = 0; i < desiredLength; i++) {
-    var charset = possibleChars[Math.floor(Math.random() * possibleChars.length)];
-    var char = charset[Math.floor(Math.Random() * charset.length)];
-    result += char;
-  }
-  return result;
-}
 
 
 // need to make sure password includes everything the user wants // 
@@ -61,4 +104,4 @@ function genPassword() {
 
 
 // Add event listener to generate button // 
-generateBtn.addEventListener("click", genPassword);
+generateBtn.addEventListener("click", generatePassword);
