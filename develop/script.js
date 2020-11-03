4181057cb8d5b33ce9a1cd43f2921decfadfe134
Kayla Lynch <kalynch1292@gmail.com>
2020-11-03 14:47:01 -0500

    floor.random error

--- develop/script.js
+++ develop/script.js
@@ -6,25 +6,25 @@ var possibleChars = [];
 
 
 // return arrayOf{Upper} -- random pick in array of each function//
-function getUpper() {
-  var arrayOfUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  return arrayOfUpper[Math.floor(Math.Random() * 26)]
-}
+// function getUpper() {
+var arrayOfUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+//   return arrayOfUpper[Math.floor(Math.Random() * 26)]
+// }
 
-function getLower() {
-  var arrayOfLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  return arrayOfLower[Math.floor(Math.random() * 26)]
-}
+// function getLower() {
+var arrayOfLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//   return arrayOfLower[Math.floor(Math.random() * 26)]
+// }
 
-function getSymbols() {
-  var arrayOfSymbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"]
-  return arrayOfSymbols[Math.floor(Math.random() * 13)]
-}
+// function getSymbols() {
+var arrayOfSymbols = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"]
+//   return arrayOfSymbols[Math.floor(Math.random() * 13)]
+// }
 
-function getNumbers() {
-  var arrayOfNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  return arrayOfNumbers[Math.floor(Math.random() * 9)]
-}
+// function getNumbers() {
+var arrayOfNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+//   return arrayOfNumbers[Math.floor(Math.random() * 9)]
+// }
 
 
 
@@ -37,7 +37,6 @@ function writePassword(h) {
 
 }
 function generatePassword() {
-  // var password = '';
 
   var desiredLength = parseInt(prompt("How long do you want your password? (Between 8-128 characters)"))
   while (desiredLength < 8 || desiredLength > 128) {
@@ -72,12 +71,14 @@ function generatePassword() {
   }
 
   for (var i = 0; i < desiredLength; i++) {
-    var charset = possibleChars[Math.floor(Math.random() * possibleChars.length)];
-    var char = charset[Math.floor(Math.Random() * charset.length)];
-    result += char;
+    var result = "";
+      var charset = possibleChars[Math.floor(Math.random() * possibleChars.length)];
+      var char = charset[Math.floor(Math.random() * charset.length)];
+     result += char;
+     return result;
   }
-  return result;
 }
+var password = generatePassword();
 
 
 
